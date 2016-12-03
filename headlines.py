d1cf16f806954adea960eb094699119697b05376
Christopher Ray Roberts <christopher.ray.roberts@outlook.com>
2016-12-03 15:31:59 -0800

    modifed to use Jinja templating

--- headlines.py
+++ headlines.py
@@ -17,16 +17,10 @@ def get_news(publication="bbc"):
     """
     feed = feedparser.parse(RSS_FEEDS[publication])
     first_article = feed['entries'][0]
-    return """<html>
-      <body>
-         <h1>Headlines</h1>
-         <b>{0}</b> <br/>
-         <i>{1}</i> <br/>
-         <p>{2}</p> <br/>
-      </body>
-    </html>""".format(first_article.get("title"),
-                      first_article.get("published"),
-                      first_article.get("summary"))
+    return render_template("home.html",
+                           title=first_article.get("title"),
+                           published=first_article.get("published"),
+                           summary=first_article.get("summary"))
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
